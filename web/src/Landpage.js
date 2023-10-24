af1bc7ba384e8f840ab32c410e381dad76047360
agusteston <agustin.teston@hotmail.com>
2023-10-24 17:53:14 -0300

    Customize courses

--- web/src/Landpage.js
+++ web/src/Landpage.js
@@ -16,7 +16,63 @@ import Container from '@mui/material/Container';
 import Copyright from './general/Copyright';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const cards = [
+  {
+    index: 1,
+    image: 'https://i.ytimg.com/vi/bYOjmW-740M/maxresdefault.jpg',
+    name: 'Curso de Desarrollo Web',
+    description: 'Aprende a crear aplicaciones web modernas con las últimas tecnologías.',
+  },
+  {
+    index: 2,
+    image: 'https://institutonoa.com.ar/wp-content/uploads/2021/10/diseno_grafico.jpg',
+    name: 'Diseño Gráfico Avanzado',
+    description: 'Perfecciona tus habilidades de diseño gráfico y crea proyectos impresionantes.',
+  },
+  {
+    index: 3,
+    image: 'https://escuelafullstack.com/web/image/slide.channel/14/image_512',
+    name: 'Machine Learning con Python',
+    description: 'Explora el mundo del machine learning y la inteligencia artificial con Python.',
+  },
+  {
+    index: 4,
+    image: 'https://i.ytimg.com/vi/sRAlwvGz-vo/maxresdefault.jpg',
+    name: 'Curso de Marketing Digital',
+    description: 'Domina las estrategias de marketing digital para hacer crecer tu negocio.',
+  },
+  {
+    index: 5,
+    image: 'https://i.ytimg.com/vi/_vzc9pQnpic/maxresdefault.jpg',
+    name: 'Programación en C++',
+    description: 'Aprende a programar en C++ y construye aplicaciones de alto rendimiento.',
+  },
+  {
+    index: 6,
+    image: 'https://d3puay5pkxu9s4.cloudfront.net/curso/4271/800_imagen.jpg',
+    name: 'Curso de Cocina Gourmet',
+    description: 'Conviértete en un chef experto y prepara platos gourmet de alta calidad.',
+  },
+  {
+    index: 7,
+    image: 'https://globaldardos.com.ar/blog/wp-content/uploads/2021/09/blog-1.png',
+    name: 'Desarrollo de Aplicaciones Móviles',
+    description: 'Crea aplicaciones móviles para iOS y Android y llega a millones de usuarios.',
+  },
+  {
+    index: 8,
+    image: 'https://inverarg.com/wp-content/uploads/2022/03/cursobolsa_2-1024x576.jpg',
+    name: 'Inversión en Bolsa',
+    description: 'Aprende a invertir en el mercado de valores y hacer crecer tu patrimonio.',
+  },
+  {
+    index: 10,
+    image: 'https://www.escueladeyoga.com/wp-content/uploads/2021/07/EIY-coemmn-004.jpg',
+    name: 'Curso de Yoga y Meditación',
+    description: 'Encuentra la paz interior y mejora tu salud a través del yoga y la meditación.',
+  }
+];
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -71,7 +127,7 @@ export default function Landpage() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+              <Grid item key={card.index} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
@@ -81,20 +137,18 @@ export default function Landpage() {
                       // 16:9
                       pt: '56.25%',
                     }}
-                    image="https://source.unsplash.com/random?wallpapers"
+                    image={card.image}
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      {card.name}
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
+                      {card.description}
                     </Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
                   </CardActions>
                 </Card>
               </Grid>
