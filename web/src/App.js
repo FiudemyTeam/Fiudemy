00ff46c6cef958c81a0639565e075959b043ed53
agusteston <agustin.teston@hotmail.com>
2023-10-25 13:04:27 -0300

    Add not found route and default

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Landpage from './Landpage';
 import LogIn from './LogIn';
+import Register from './Register';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogIn />}></Route>
-        <Route path="/home" element={<Landpage />}></Route>    
+        <Route path="" element={<Navigate to="/login" />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/home" element={<Landpage />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>    
       </Routes>  
     </BrowserRouter>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div style={{ marginLeft: '20px' }}>
+      <h1>404 Page not found</h1>
+    </div>
   );
 }
 
