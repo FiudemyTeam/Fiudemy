acca602c2e05305cf787ec7088f8455a3d603e53
clezious <manuelreberendo@gmail.com>
2023-11-01 18:04:55 -0300

    Added UserDependency - This gets the requester user with it's token - Fixed default user password

---
+++ api/dependencies.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, status
+from typing import Annotated
+from auth import auth_handler
+from repositories.user_repository import find_user
+from models import User
+
+
+def get_current_user(decoded_token: str = Depends(auth_handler.auth_wrapper)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials'
+    )
+    username = decoded_token
+    if username is None:
+        raise credentials_exception
+    user = find_user(username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+UserDependency = Annotated[User, Depends(get_current_user)]
