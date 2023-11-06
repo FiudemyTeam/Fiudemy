b8cff72c8ae34de21e4884bcaf76251e189816a8
TDPFIUBA <tdelpup@fi.uba.ar>
2023-11-06 11:52:20 -0300

    removed unused console log

--- web/src/pages/Landpage/components/Grid/api/index.jsx
+++ web/src/pages/Landpage/components/Grid/api/index.jsx
@@ -4,7 +4,6 @@ const API_HOST = import.meta.env.VITE_API_HOST;
 
 export async function fetchCourses(params) {
   try {
-    console.log(params);
     const response = await axios.get(`${API_HOST}/courses/`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
