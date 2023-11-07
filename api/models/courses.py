e56edc5b4cfd58185fe9e823932d7a4a9e41523c
clezious <manuelreberendo@gmail.com>
2023-11-07 02:08:55 -0300

    Added courseMaterial model and table

--- api/models/courses.py
+++ api/models/courses.py
@@ -3,6 +3,7 @@ from sqlmodel import Field, SQLModel, Relationship
 
 from .course_rates import CourseUserRate
 from .course_favorites import CourseUserFavorite
+from .course_materials import CourseMaterial
 
 if TYPE_CHECKING:
     from .users import User
@@ -21,6 +22,7 @@ class Course(CourseBase, table=True):
                                             link_model=CourseUserRate)
     user_favorites: List["User"] = Relationship(back_populates="course_favorites",
                                                 link_model=CourseUserFavorite)
+    course_materials: List["CourseMaterial"] = Relationship(back_populates="course")
     category_id: Optional[int] = Field(default=None)
 
 
@@ -30,3 +32,7 @@ class CourseCreate(CourseBase):
 
 class CourseRead(CourseBase):
     id: int
+
+
+class CourseReadWithMaterials(CourseRead):
+    course_materials: List[CourseMaterial] = []
