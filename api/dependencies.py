b6e51d8449afcb21feb41e9686c104028cd1976e
clezious <manuelreberendo@gmail.com>
2023-11-07 01:51:23 -0300

    Added session dependency - This replaces current with(...) blocks - user_repository functions fixed to receive session

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,11 +1,20 @@
 from fastapi import Depends, HTTPException, status
 from typing import Annotated
 from auth import auth_handler
+from sqlmodel import Session
+
 from repositories.user_repository import find_user
 from models.users import User
+from db import engine
 
 
-def get_current_user(decoded_token: str = Depends(auth_handler.auth_wrapper)):
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def get_current_user(decoded_token: str = Depends(auth_handler.auth_wrapper),
+                     session: Session = Depends(get_session)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Could not validate credentials'
@@ -13,7 +22,7 @@ def get_current_user(decoded_token: str = Depends(auth_handler.auth_wrapper)):
     username = decoded_token
     if username is None:
         raise credentials_exception
-    user = find_user(username)
+    user = find_user(session, username)
     if user is None:
         raise credentials_exception
     return user
--- api/routers/courses.py
+++ api/routers/courses.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, HTTPException, Response
-from pydantic import BaseModel
-from sqlmodel import Session, select, and_
+from fastapi import APIRouter, HTTPException, Response, Depends
+from sqlmodel import select, and_, Session
 from typing import List, Optional
 
-from models.courses import Course, CourseCreate, CourseRead, CourseUserRate, CourseUserFavorite
-from dependencies import UserDependency
-from db import engine
+from models.courses import (
+    Course, CourseCreate, CourseRead, CourseUserRate,
+    CourseUserFavorite, CourseReadWithMaterials
+)
+from dependencies import UserDependency, get_session
+
 
 router = APIRouter(
     prefix="/courses",
@@ -20,71 +22,71 @@ async def get_courses(
     searchString: Optional[str] = None,
     rate: Optional[int] = None,
     favorite: Optional[bool] = None,
+    session: Session = Depends(get_session)
 ):
-    with Session(engine) as session:
-        query = select(Course)
-        filters = []
+    query = select(Course)
+    filters = []
 
-        if category:
-            filters.append(Course.category_id == category)
+    if category:
+        filters.append(Course.category_id == category)
 
-        if searchString:
-            filters.append(Course.name.like(f"%{searchString}%"))
+    if searchString:
+        filters.append(Course.name.like(f"%{searchString}%"))
 
-        if rate is not None:
-            filters.append(Course.user_rates.any(CourseUserRate.rate == rate))
+    if rate is not None:
+        filters.append(Course.user_rates.any(CourseUserRate.rate == rate))
 
-        if favorite:
-            filters.append(Course.user_favorites.any(
-                CourseUserFavorite.favorite == favorite))
+    if favorite:
+        filters.append(Course.user_favorites.any(
+            CourseUserFavorite.favorite == favorite))
 
-        query = query.where(and_(*filters))
-        results = session.exec(query)
-        return results.all()
+    query = query.where(and_(*filters))
+    results = session.exec(query)
+    return results.all()
 
 
-@router.get("/{id}", response_model=CourseRead)
-async def get_course(id: int):
-    with Session(engine) as session:
-        course = session.get(Course, id)
-        if not course:
-            raise HTTPException(status_code=404, detail="Course not found")
-        return course
+@router.get("/{id}", response_model=CourseReadWithMaterials)
+async def get_course(id: int,
+                     session: Session = Depends(get_session)):
+    course = session.get(Course, id)
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return course
 
 
 @router.post("/", response_model=CourseRead)
-def post_course(course: CourseCreate):
-    with Session(engine) as session:
-        session.add(new_course := Course.from_orm(course))
-        session.commit()
-        session.refresh(new_course)
-        return new_course
+def post_course(course: CourseCreate,
+                session: Session = Depends(get_session)):
+    session.add(new_course := Course.from_orm(course))
+    session.commit()
+    session.refresh(new_course)
+    return new_course
 
 
 @router.post("/{id}/rate", response_model=CourseUserRate, status_code=200)
 def upsert_course_rate(id: int,
                        rate: int,
                        user: UserDependency,
-                       response: Response):
-    with Session(engine) as session:
-        # Check if rate already exists
-        statement = select(CourseUserRate).where(
-            CourseUserRate.user_id == user.id,
-            CourseUserRate.course_id == id)
-        course_rate = session.exec(statement).first()
+                       response: Response,
+                       session: Session = Depends(get_session)):
+    # Check if rate already exists
+    statement = select(CourseUserRate).where(
+        CourseUserRate.user_id == user.id,
+        CourseUserRate.course_id == id)
+    course_rate = session.exec(statement).first()
 
-        # If not, create a new one
-        if course_rate is None:
-            course_rate = CourseUserRate(user_id=user.id,
-                                         course_id=id,
-                                         rate=rate)
-            response.status_code = 201
-        # Else, update the existing one
-        else:
-            course_rate.rate = rate
+    # If not, create a new one
+    if course_rate is None:
+        course_rate = CourseUserRate(user_id=user.id,
+                                     course_id=id,
+                                     rate=rate)
+        response.status_code = 201
+    # Else, update the existing one
+    else:
+        course_rate.rate = rate
 
-        session.add(course_rate)
-        session.commit()
-        session.refresh(course_rate)
+    session.add(course_rate)
+    session.commit()
+    session.refresh(course_rate)
 
-        return course_rate
+    return course_rate
