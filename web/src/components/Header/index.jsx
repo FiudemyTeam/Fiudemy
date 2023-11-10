e5b52858d2a366a034e004f57620e2368274f68b
agusteston <agustin.teston@hotmail.com>
2023-11-10 20:54:07 -0300

    Add profesor profile button in toolbar

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -3,10 +3,10 @@ import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import { Link } from "react-router-dom";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import PersonIcon from "@mui/icons-material/Person";
 import SearchBar from "./components/SearchBar";
 import LogoutButton from "./components/LogoutButton";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 
 const Header = () => {
   return (
@@ -33,10 +33,18 @@ const Header = () => {
         </Link>
         <div style={{ flexGrow: 1 }} />
         <SearchBar />
+        <Link
+          to="/profesor"
+          style={{ textDecoration: "none", color: "white", marginRight: 20 }}
+          title="Perfil Profesor"
+        >
+          <LibraryBooksIcon />
+        </Link>
         <div style={{ display: "flex", alignItems: "center" }}>
           <Link
             to="/profile"
             style={{ textDecoration: "none", color: "white", marginRight: 20 }}
+            title="Perfil Alumno"
           >
             <PersonIcon />
           </Link>
