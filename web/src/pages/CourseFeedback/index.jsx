94bf3e5e52c6d73b62da16bbd39fb64f5f1d35c8
TDPFIUBA <tdelpup@fi.uba.ar>
2023-11-18 03:58:29 -0300

    fixed course material checkbox & review only on course completed

--- web/src/pages/CourseFeedback/index.jsx
+++ web/src/pages/CourseFeedback/index.jsx
@@ -1,21 +1,22 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Rating from '@mui/material/Rating';
-import Button from '@mui/material/Button';
-import { Container } from '@mui/material';
+import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
+import Button from "@mui/material/Button";
+import { Container, Box } from "@mui/material";
 import axios from "axios";
+import { CourseContext } from "@context/CourseContext";
 
 const API_HOST = import.meta.env.VITE_API_HOST;
 
 const ReviewView = () => {
   const { id } = useParams();
+  const { isCompleted } = useContext(CourseContext);
 
   const fetchReviews = async () => {
     try {
-      const response = await axios.get(`${API_HOST}/courses/${id}/rate`, 
-      {
+      const response = await axios.get(`${API_HOST}/courses/${id}/rate`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -23,10 +24,10 @@ const ReviewView = () => {
       if (response.status === 200) {
         setReviews(response.data);
       } else {
-        console.error('Error al obtener reseñas del backend');
+        console.error("Error al obtener reseñas del backend");
       }
     } catch (error) {
-      console.error('Error de red', error);
+      console.error("Error de red", error);
     }
   };
 
@@ -35,7 +36,7 @@ const ReviewView = () => {
   }, []);
 
   const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const [reviews, setReviews] = useState([]);
 
   const handleRatingChange = (event, newValue) => {
@@ -47,66 +48,89 @@ const ReviewView = () => {
   };
 
   const handleSubmitReview = async () => {
-    if (rating > 0 && comment.trim() !== '') {
+    if (rating > 0 && comment.trim() !== "") {
       const newReview = {
         rate: rating,
         comment: comment,
       };
 
       try {
-        const response = await axios.post(`${API_HOST}/courses/${id}/rate`, 
-        newReview, 
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
+        const response = await axios.post(
+          `${API_HOST}/courses/${id}/rate`,
+          newReview,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
 
         if (response.status === 200 || response.status === 201) {
           // La reseña fue creada con éxito en el backend
           await fetchReviews();
           setRating(0);
-          setComment('');
+          setComment("");
         } else {
           // Manejar otros casos, si es necesario
-          console.error('Error al crear la reseña en el backend');
+          console.error("Error al crear la reseña en el backend");
         }
       } catch (error) {
-        console.error('Error de red', error);
+        console.error("Error de red", error);
       }
     }
-  }
+  };
 
   return (
     <div>
-      <Container sx={{ marginTop: "20px" }}>
-      <Typography variant="h4">Opiniones de curso</Typography>
-    <div><br></br>
-      <Rating name="rating" value={rating} onChange={handleRatingChange} />
-      <TextField
-        label="Comentario"
-        value={comment}
-        onChange={handleCommentChange}
-        multiline
-        rows={4}
-        fullWidth
-      />
-  <br></br><br></br>
-  <Button variant="contained" onClick={handleSubmitReview}>
-    Enviar Reseña
-  </Button>
-</div>
-<br></br>
-<br></br>
-<div>
+      <Container maxWidth="md" sx={{ marginTop: "20px" }}>
+        <Typography variant="h4" paddingY={2}>
+          Opiniones de curso
+        </Typography>
+        {isCompleted && (
+          <div>
+            <Rating
+              name="rating"
+              value={rating}
+              onChange={handleRatingChange}
+            />
+            <TextField
+              label="Comentario"
+              value={comment}
+              onChange={handleCommentChange}
+              multiline
+              rows={4}
+              fullWidth
+            />
 
-{reviews.map((review) => (
-        <div key={review.course_id + "-" + review.user_id} style={{ border: "1px solid #ccc", borderRadius: "10px", padding: "10px", margin: "10px" }}>
-          <Typography variant="h6">Puntuación: {review.rate}</Typography>
-          <Typography variant="body1">{review.comment}</Typography>
+            <Button
+              variant="contained"
+              onClick={handleSubmitReview}
+              sx={{ marginY: "15px" }}
+            >
+              Enviar Reseña
+            </Button>
+          </div>
+        )}
+        <div>
+          {reviews.map((review) => (
+            <div
+              key={review.course_id + "-" + review.user_id}
+              style={{
+                border: "1px solid #ccc",
+                borderRadius: "10px",
+                padding: "10px",
+              }}
+            >
+              <Typography variant="h6" display="flex" alignItems="center">
+                Puntuación:
+                <Box ml={1} display="flex" alignItems="flex-end">
+                  <Rating name="rating" value={review.rate} readOnly />
+                </Box>
+              </Typography>
+              <Typography variant="body1">{review.comment}</Typography>
+            </div>
+          ))}
         </div>
-))}
-</div>
       </Container>
     </div>
   );
