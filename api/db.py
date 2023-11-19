6b2a438bb91f91322781721c053b9b1d2950c76a
josefinacairo <mcairo@fi.uba.ar>
2023-11-19 19:24:57 -0300

    Added more materials to courses

--- api/db.py
+++ api/db.py
@@ -46,12 +46,156 @@ def seed_db():
         ),
         CourseMaterial(
             id=3,
+            title='Introducción a CSS',
+            description='Introducción a la estilización de páginas web con CSS.',
+            order=3,
+            type='video',
+            value='https://www.youtube.com/watch?v=24gNhTcy6pw',
+            course_id=1
+        ),
+        CourseMaterial(
+            id=4,
+            title='Responsive Web Design',
+            description='Introducción a la creación de diseños web que se adaptan a diferentes dispositivos y tamaños de pantalla.Media queries y diseño fluido',
+            order=4,
+            type='video',
+            value='https://www.youtube.com/watch?v=2zypRlzIcHc',
+            course_id=1
+        ),
+        CourseMaterial(
+            id=5,
+            title='Introducción a Git y Control de Versiones',
+            description='Principios básicos de Git y GitHub para el control de versiones.',
+            order=5,
+            type='video',
+            value='https://www.youtube.com/watch?v=3GymExBkKjE',
+            course_id=1
+        ),   
+        CourseMaterial(
+            id=6,
+            title='Principios Básicos de Diseño Web',
+            description='Conceptos fundamentales de diseño como la tipografía, el color y la disposición.',
+            order=6,
+            type='video',
+            value='https://www.youtube.com/watch?v=-ut593BSlCk',
+            course_id=1
+        ),    
+        CourseMaterial(
+            id=7,
+            title='Introducción a Node.js y Express (Backend)',
+            description='Breve vista al desarrollo del lado del servidor con Node.js. Configuración de un servidor simple con Express.',
+            order=7,
+            type='video',
+            value='https://www.youtube.com/watch?v=1hpc70_OoAg',
+            course_id=1
+        ),      
+        CourseMaterial(
+            id=8,
+            title='Despliegue de Aplicaciones Web',
+            description='Opciones para desplegar aplicaciones web en la nube o en servicios de alojamiento.',
+            order=8,
+            type='video',
+            value='https://www.youtube.com/watch?v=xZjIMrmu-bg',
+            course_id=1
+        ),                                        
+        CourseMaterial(
+            id=9,
             title='Introducción a Adobe Photoshop',
             description='Aprende los conceptos básicos de Adobe Photoshop y domina el retoque de imágenes.',
             order=1,
             type='video',
             value='https://www.youtube.com/watch?v=abc123',
             course_id=2
+        ),
+        CourseMaterial(
+            id=12,
+            title='Tipografía Avanzada',
+            description='Exploración de técnicas avanzadas de manipulación y diseño de tipografía.Combinación y emparejamiento de fuentes para lograr impacto visual. Diseño tipográfico para diferentes formatos y plataformas',
+            order=2,
+            type='video',
+            value='https://www.youtube.com/watch?v=3_v47BU0wDg',
+            course_id=2
+        ),   
+        CourseMaterial(
+            id=10,
+            title='Diseño de Identidad de Marca',
+            description='Desarrollo de identidades visuales coherentes y distintivas. Creación de logotipos y sistemas de marca. Estrategias de diseño para transmitir la personalidad y los valores de una marca.',
+            order=3,
+            type='video',
+            value='https://www.youtube.com/watch?v=88kNe9dthDU',
+            course_id=2
+        ),
+        CourseMaterial(
+            id=11,
+            title='Ilustración Digital Avanzada',
+            description='Técnicas de ilustración vectorial y diseño de gráficos complejos. Integración de ilustraciones en proyectos de diseño gráfico.',
+            order=4,
+            type='video',
+            value='https://www.youtube.com/watch?v=-yUMCH5vVnk',
+            course_id=2
+        ),             
+        CourseMaterial(
+            id=13,
+            title='Diseño Editorial y Maquetación',
+            description='Maquetación de revistas, libros y otros materiales editoriales. Consideraciones de diseño para la legibilidad y la experiencia del usuario.',
+            order=5,
+            type='video',
+            value='https://www.youtube.com/watch?v=B66NWPBPR3o',
+            course_id=2
+        ),
+        CourseMaterial(
+            id=14,
+            title='Animación y Diseño de Interfaz de Usuario (UI)',
+            description='Diseño de interfaces de usuario animadas. Herramientas y técnicas para crear prototipos interactivos.',
+            order=6,
+            type='video',
+            value='https://www.youtube.com/watch?v=PnhcSBk6Spw',
+            course_id=2
+        ),   
+        CourseMaterial(
+            id=15,
+            title='Introducción al Marketing Digital',
+            description='Definición de Marketing Digital. Importancia y beneficios del Marketing Digital. Breve historia y evolución del Marketing Digital.',
+            order=1,
+            type='video',
+            value='https://www.youtube.com/watch?v=JVZmN1OSld4',
+            course_id=4
+        ),
+        CourseMaterial(
+            id=16,
+            title='Estrategias de Contenido y Marketing de Contenidos',
+            description='Importancia del contenido en el marketing. Estrategias de marketing de contenidos. Creación y distribución de contenido relevante.',
+            order=2,
+            type='video',
+            value='https://www.youtube.com/watch?v=os9TWBSvVSA',
+            course_id=4
+        ),
+        CourseMaterial(
+            id=17,
+            title='SEO (Optimización para Motores de Búsqueda)',
+            description='Introducción al SEO y su importancia. Factores de clasificación en los motores de búsqueda. Estrategias para mejorar el posicionamiento web.',
+            order=3,
+            type='video',
+            value='https://www.youtube.com/watch?v=wHmFzsB22Tg',
+            course_id=4
+        ),
+        CourseMaterial(
+            id=18,
+            title='Publicidad en Redes Sociales',
+            description='Estrategias de publicidad en plataformas sociales. Segmentación de audiencia y personalización de anuncios. Análisis de métricas y rendimiento publicitario.',
+            order=4,
+            type='video',
+            value='https://www.youtube.com/watch?v=shfbXfBLDbc',
+            course_id=4
+        ),
+        CourseMaterial(
+            id=19,
+            title='Analítica Web y Marketing de Conversión',
+            description='Herramientas de analítica web (Google Analytics). Interpretación de datos y métricas clave. Estrategias para aumentar la tasa de conversión.',
+            order=5,
+            type='video',
+            value='https://www.youtube.com/watch?v=obpdQ4f7A0I',
+            course_id=4
         )
     ]
 
