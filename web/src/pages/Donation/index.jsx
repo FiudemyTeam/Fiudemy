554b378c0741e176c6c53355398eaca55854914b
Agustin <amoralejo@fi.uba.ar>
2023-11-20 11:19:26 -0300

    refactor donation view

--- web/src/pages/Donation/index.jsx
+++ web/src/pages/Donation/index.jsx
@@ -1,110 +1,116 @@
-import React, { useState } from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import AddressForm from '../Donation/AddresForm';
+import PaymentForm from '../Donation/PaymentForm';
+import Review from '../Donation/Review';
 
-const Donation = () => {
-  const [amount, setAmount] = useState(0);
-  const [message, setMessage] = useState("");
-  const [donationSent, setDonationSent] = useState(false);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link to="/home">
+        Fiudemy
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  const handleAmountChange = (event) => {
-    setAmount(event.target.value);
+const steps = ['Dedicatoria', 'Donacion', 'Detalles de Pago'];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 2:
+      return <PaymentForm />;
+    case 1:
+      return <Review />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
+
+export default function Checkout() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
   };
 
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-  };
-
-  const handleDonate = () => {
-    // Aquí puedes realizar acciones relacionadas con la donación (por ejemplo, enviar datos al servidor)
-    console.log(`Donación de ${amount} recibida con el mensaje: ${message}`);
-
-    // Actualizar el estado para indicar que la donación se ha enviado con éxito
-    setDonationSent(true);
-
-    // Limpiar los campos del formulario después de la donación
-    setAmount(0);
-    setMessage("");
-  };
-
-  const handleNewDonation = () => {
-    // Restablecer el estado para permitir una nueva donación
-    setDonationSent(false);
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "20px",
-        marginTop: "20px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-        borderRadius: "8px",
-        backgroundColor: "#ffffff",
-      }}
-    >
-      <Typography variant="h4" component="h1" sx={{ marginBottom: "20px" }}>
-        Gracias por colaborar con el curso
-      </Typography>
-      {donationSent ? (
-        <div>
-          <Typography variant="h6" color="primary" sx={{ marginBottom: "20px" }}>
-            ¡Gracias por tu donación!
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        color="default"
+        elevation={0}
+        sx={{
+          position: 'relative',
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}
+      >
+      </AppBar>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            Tu Donacion
           </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleNewDonation}
-            sx={{ marginTop: "20px" }}
-          >
-            Nueva Donación
-          </Button>
-        </div>
-      ) : (
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField
-              label="Monto"
-              type="number"
-              variant="outlined"
-              fullWidth
-              value={amount}
-              onChange={handleAmountChange}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField
-              label="Mensaje"
-              variant="outlined"
-              multiline
-              rows={4}
-              fullWidth
-              value={message}
-              onChange={handleMessageChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleDonate}
-              sx={{ marginTop: "20px" }}
-            >
-              Donar
-            </Button>
-          </Grid>
-        </Grid>
-      )}
-    </Box>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography variant="h5" gutterBottom>
+                Gracias por tu donacion!.
+              </Typography>
+              <Typography variant="subtitle1">
+                Los fondos donados de dirigiran directamente a la billetera del profesor a cargo del curso.
+              </Typography>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Volver
+                  </Button>
+                )}
+
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  {activeStep === steps.length - 1 ? 'Donar' : 'Siguiente'}
+                </Button>
+              </Box>
+            </React.Fragment>
+          )}
+        </Paper>
+        <Copyright />
+      </Container>
+    </React.Fragment>
   );
-};
-
-export default Donation;
-
-
+}
